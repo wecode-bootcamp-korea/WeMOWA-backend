60e6229aecc5d58e827e26a899cf92c2175c4625
misun9283 <kimmisun9283@naver.com>
2020-05-28 18:55:00 +0900

    [fix]account 앱 models.py 수정 - user와 address의 간의 관계를 onetomany로 수정

--- account/models.py
+++ account/models.py
@@ -5,35 +5,28 @@ from product.models import *
 class User(models.Model):
 	first_name	= models.CharField(max_length = 100)
 	last_name	= models.CharField(max_length = 100)
-	email		= models.CharField(max_length = 200)
-	password	= models.CharField(max_length = 100)
+	email		= models.EmailField(max_length = 200)
+	password	= models.CharField(max_length = 300)
 	prefix		= models.ForeignKey('Prefix',on_delete = models.SET_NULL, null=True)
 	created_at	= models.DateTimeField(auto_now_add = True)
 	updated_at	= models.DateTimeField(auto_now = True)
 	country		= models.CharField(max_length = 50)
-	address		= models.ManyToManyField('Address', through = 'UserAddress')
 	wishlist    = models.ManyToManyField(Product,through = 'UserWishlist')
 
 	class Meta:
 		db_table = 'users'
 
 class Address(models.Model):
-	name		= models.CharField(max_length = 50)
-	st_addr		= models.CharField(max_length = 300)
-	op_addr		= models.CharField(max_length = 500, null=True, blank = True)
-	city		= models.CharField(max_length = 50)
-	zip_code	= models.CharField(max_length = 50)
-	phone		= models.CharField(max_length = 50)
+	name		    = models.CharField(max_length = 50)
+	st_address		= models.CharField(max_length = 300)
+	op_address		= models.CharField(max_length = 500, null=True, blank = True)
+	city		    = models.CharField(max_length = 50)
+	zip_code	    = models.CharField(max_length = 50)
+	phone_number    = models.CharField(max_length = 50)
+    user            = models.ForeignKey('User',on_delete = models.SET_NULL, null=True)
 
 	class Meta:
 		db_table = 'addresses'
-
-class UserAddress(models.Model):
-	user		= models.ForeignKey('User',on_delete = models.SET_NULL, null=True)
-	address		= models.ForeignKey('Address', on_delete = models.SET_NULL, null = True)
-	
-	class Meta:
-		db_table = 'users_addresses'
 
 class Prefix(models.Model):
 	name		= models.CharField(max_length = 50)
