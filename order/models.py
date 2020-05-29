744ed5b9ffee85044c4055d609b6c4faf64ee531
Raina Kim <hrk296@nyu.edu>
2020-05-29 23:19:21 +0900

    align

--- order/models.py
+++ order/models.py
@@ -3,29 +3,29 @@ from account.models import User
 from product.models import  Product, Tag, Handle, Wheel
 
 class Order(models.Model):
-	user			= models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
-	order_status	= models.ForeignKey('OrderStatus', on_delete = models.SET_NULL, null = True)
-	subtotal		= models.DecimalField(max_digits=10, decimal_places=2)
-	created_at		= models.DateTimeField(auto_now_add = True)
-	product			= models.ManyToManyField(Product, through = 'Cart')
+    user         = models.ForeignKey(User, on_delete = models.SET_NULL, null=True)
+    order_status = models.ForeignKey('OrderStatus', on_delete = models.SET_NULL, null = True)
+    subtotal     = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at   = models.DateTimeField(auto_now_add = True)
+    product      = models.ManyToManyField(Product, through = 'Cart')
 
-	class Meta:
-		db_table	= 'orders'
+    class Meta:
+        db_table = 'orders'
 
 class OrderStatus(models.Model):
-	name		= models.CharField(max_length = 50)
+    name = models.CharField(max_length = 50)
 
-	class Meta:
-		db_table	= 'orders_status'
+    class Meta:
+        db_table = 'orders_status'
 
 class Cart(models.Model):
-	order			= models.ForeignKey('Order', on_delete = models.SET_NULL, null= True)
-	product			= models.ForeignKey(Product, on_delete = models.SET_NULL, null = True)
-	amount			= models.IntegerField()
-	tag				= models.ForeignKey(Tag, on_delete = models.SET_NULL, null = True, blank = True)
-	handle			= models.ForeignKey(Handle, on_delete = models.SET_NULL, null = True, blank = True)
-	wheel			= models.ForeignKey(Wheel, on_delete = models.SET_NULL, null = True, blank = True)
-	
-	class Meta:
-		db_table	= 'carts'
+    order   = models.ForeignKey('Order', on_delete = models.SET_NULL, null= True)
+    product = models.ForeignKey(Product, on_delete = models.SET_NULL, null = True)
+    amount  = models.IntegerField()
+    tag     = models.ForeignKey(Tag, on_delete = models.SET_NULL, null = True, blank = True)
+    handle  = models.ForeignKey(Handle, on_delete = models.SET_NULL, null = True, blank = True)
+    wheel   = models.ForeignKey(Wheel, on_delete = models.SET_NULL, null = True, blank = True)
+
+    class Meta:
+        db_table	= 'carts'
 
