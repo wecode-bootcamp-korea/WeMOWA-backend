a0fbc327bc97d17590c19e41191118ae72e858e0
Raina Kim <hrk296@nyu.edu>
2020-06-01 15:56:28 +0900

    [Add, Fix] Product/Account Models/Views - Product Models (color table deleted and merged with product table) - Product Views (all-luggage lists with sorting available) - Account Models (made email and password field unique) - Account views (signup, signin implementation and test API with   frontend completed) - Migrations Error fixed (deleted entire migration files and remigrated) - Added urls.py for each app

--- account/views.py
+++ account/views.py
@@ -1,3 +1,50 @@
+import json
+from django.views import View
+from django.http import JsonResponse, HttpResponse
+from .models import User
+from .models import Prefix
 from django.shortcuts import render
+from django.db import IntegrityError
+import bcrypt
+import jwt
+from my_settings import SECRET_KEY
 
-# Create your views here.
+class SignUpView(View):
+    def post(self,request):
+        try:
+            data = json.loads(request.body)
+            password = bcrypt.hashpw(data['password'].encode('utf-8'),bcrypt.gensalt())
+            crypted = password.decode('utf-8')
+            User.objects.create(
+                first_name = data['first_name'],
+                last_name = data['last_name'],
+                email = data['email'],
+                password = crypted,
+                country = data['country'],
+                prefix = Prefix.objects.get(name = data['prefix'])
+            )
+            return HttpResponse(status = 200)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'},status = 400)
+        except IntegrityError:
+            return JsonResponse({'message':'DUPLICATE_ENTRIES'},status = 400)
+
+class SignInView(View):
+    def post(self,request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(email = data['email']).exists():
+                user = User.objects.get(email = data['email'])
+                if bcrypt.checkpw(data['password'].encode('utf-8'),user.password.encode('utf-8')):
+                    token = jwt.encode({'email':data['email']}, SECRET_KEY['SECRET_KEY'], algorithm = 'HS256')
+                    token = token.decode('utf-8')
+                    return JsonResponse({'token':token}, status = 200)
+                else:
+                    return JsonResponse({'message':'INVALID_USER'}, status = 401)
+            else:
+                return JsonResponsse({'message':'INVALID_USER'}, status = 401)
+        except KeyError:
+            return JsonResponse({'message':'INVALID_KEYS'}, status = 400)
+
+
+
--- product/views.py
+++ product/views.py
@@ -1,3 +1,44 @@
+mport json
 from django.shortcuts import render
+from django.view import View
+from .models import *
+from django.http import HttpResponse, JsonResponse
 
-# Create your views here.
+
+class ProductListView(View):
+    def get(self,request):
+        sort = request.GET('sort',None)
+        if sort == 'PRICE LOW TO HIGH':
+            product_list = list(Product.objects.values().order_by('price'))
+        elif sort == 'PRICE HIGH TO LOW':
+            product_list = list(Product.objects.values().order_by('-price'))
+        elif sort == 'A-Z':
+            product_list = list(Product.objects.values().order_by('name'))
+        elif sort == 'Z-A':
+            product_list = list(Product.objects.values().order_by('-name'))
+        else:
+            product_list = list(Produc.objects.values())
+#        filter_options = request.GET('filter',None)
+
+        product = {}
+        series_color = {}
+        for product in product_lst:
+            collection = Collection.objects.get(id=collection_id).name
+            name = product['name']
+            price = product['price']
+            color = product['luggage_color']
+            for obj in Product.objects.filter(name = name and collection_id = collection_id):
+                series_color[obj.luggage_color] = [obj.color_url]
+                images = Image.objects.filter(product_id = obj.id)
+                for i in range(2):
+                    series_color[obj.luggage_color].append(images[i].img_url)
+           product['collection'] = collection
+           product['name'] = name
+           product['price'] = price
+           product['luggage_color'] = color
+           product['series_color'] = series_color
+        return JsonResponse({'data':product}, status = 200)
+
+
+
+
