a02c19d78b983a498296e50b42c6bc31080c9f2c
Raina Kim <hrk296@nyu.edu>
2020-06-02 19:13:11 +0900

    [Fix] ProductListView Filter, Product urls.py
    - ProductListView filter 기능 추가
    - Product urls.py Rest API에 맞게 수정
    - Migration 오류나서 migration file지우고 remigration

--- db_uploader.py
+++ db_uploader.py
@@ -13,52 +13,58 @@ CSV_PATH_PRODUCT = '../selenium/product1.csv'
 
 with open(CSV_PATH_PRODUCT) as in_file:
     data_reader = csv.reader(in_file)
-    next(data_reader,None)
-#   악세사리 데이터 
-#    for row in data_reader:
-#        if row:
-#            stock = row[3]
-#            collection = row[0]
-#            name = row[1]
-#            price = int(row[2].split(',')[0])
-#            description = row[4]
-#            category_id = Category.objects.get(name = row[0]).id
-#            collection_id = Collection.objects.get(name = row[0]).id
-#            stock_status_id = StockStatus.objects.get(name = stock).id
-#            # Product.objects.create(category_id = category_id,collection_id = collection_id, name = name, price = price,stock_status_id = stock_status_id,description = description)
-#            image = row[5][2:len(row[5])-2]
-#            images=image.split("', '")
-#            product_id = Product.objects.get(name = row[1]).id
-#            for img in images:
-#                Image.objects.create(img_url = img, product_id = product_id)
+    next(data_reader,None) 
+    # for row in data_reader:
+    #     if row:
+            # stock = row[3]
+            # collection = row[0]
+            # name = row[1]
+            # price = int(row[2].split(',')[0])
+            # description = row[4]
+            # category_id = Category.objects.get(name = row[0]).id
+            # collection_id = Collection.objects.get(name = row[0]).id
+            # stock_status_id = StockStatus.objects.get(name = stock).id
+            # Product.objects.create(category_id = category_id,collection_id = collection_id, name = name, price = price,stock_status_id = stock_status_id,description = description)
+        #    image = row[5][2:len(row[5])-2]
+        #    images=image.split("', '")
+        #    product_id = Product.objects.get(name = row[1]).id
+        #    for img in images:
+        #        Image.objects.create(img_url = img, product_id = product_id)
+    
+    # for row in data_reader:
+    #     if row:
+    #         collection = row[0]
+    #         name = row[1]
+    #         price = int(row[2].split(',')[0].replace('.',""))
+    #         stock = row[3]
+    #         stock_status_id = StockStatus.objects.get(name = stock).id
+    #         description = row[4]
+    #         category_id = Category.objects.get(id=1).id
+    #         collection_id = Collection.objects.get(name = row[0]).id
+    #         detail = row[9]
+    #         image_string = row[5][2:len(row[5])-2]
+    #         images = image_string.split("', '")
+    #         luggage_color =row[6]
+    #         product_number = row[10]
+    #         color_urls = row[7]
+    #         product_number = row[10]
+    #         try:
+    #             texture_id = Texture.objects.get(name = row[8].capitalize()).id
+    #             color_url_eval = eval(color_urls)
+    #             color_url = color_url_eval[row[6]]
+    #         except:
+    #             texture_id = None
+    #             color_url_eval = None
+    #             color_url = None
+    #         Product.objects.create(category_id=category_id,collection_id = collection_id,stock_status_id = stock_status_id,name = name, price = price, description = description,detail = detail,texture_id = texture_id,color_url = color_url,luggage_color=luggage_color, product_number = product_number)
 
     for row in data_reader:
         if row:
-            collection = row[0]
-            name = row[1]
-            price = int(row[2].split(',')[0].replace('.',""))
-            stock = row[3]
-            stock_status_id = StockStatus.objects.get(name = stock).id
-            description = row[4]
-            category_id = Category.objects.get(id=1).id
-            collection_id = Collection.objects.get(name = row[0]).id
-            detail = row[9]
+            
+            product_id = Product.objects.get(product_number = row[10]).id
             image_string = row[5][2:len(row[5])-2]
             images = image_string.split("', '")
-            luggage_color =row[6]
-           # color_url_string = row[7][1:len(row[7])-1]
-           # color_urls = color_url_string.split(', ')
-            color_urls = row[7]
-            product_number = row[10]
-            product_id = Product.objects.get(product_number = row[10]).id 
-            try:
-                texture_id = Texture.objects.get(name = row[8].capitalize()).id
-                color_url_eval = eval(color_urls)
-                color_url = color_url_eval[row[6]]
-            except:
-                texture_id = None
-                color_url_eval = None
-                color_url = None
+
             for img in images:
                 Image.objects.create(img_url = img, product_id = product_id)
-                Product.objects.create(category_id=category_id,collection_id = collection_id,stock_status_id = stock_status_id,name=name, price = price, description = description,detail = detail,texture_id = texture_id,color_url = color_url,luggage_color=luggage_color,product_number = product_number)
+            
--- product/views.py
+++ product/views.py
@@ -1,43 +1,74 @@
-mport json
+import json
 from django.shortcuts import render
-from django.view import View
+from django.views import View
 from .models import *
 from django.http import HttpResponse, JsonResponse
 
 
 class ProductListView(View):
-    def get(self,request):
-        sort = request.GET('sort',None)
+    def get(self,request,category_id):
+        sort = request.GET.get('sort',None)
         if sort == 'PRICE LOW TO HIGH':
-            product_list = list(Product.objects.values().order_by('price'))
+            product_list = Product.objects.select_related('collection').all().order_by('price')
         elif sort == 'PRICE HIGH TO LOW':
-            product_list = list(Product.objects.values().order_by('-price'))
+            product_list = Product.objects.select_related('collection').all().order_by('-price')
         elif sort == 'A-Z':
-            product_list = list(Product.objects.values().order_by('name'))
+            product_list = Product.objects.select_related('collection').all().order_by('name')
         elif sort == 'Z-A':
-            product_list = list(Product.objects.values().order_by('-name'))
+            product_list = Product.objects.select_related('collection').all().order_by('-name')
         else:
-            product_list = list(Produc.objects.values())
-#        filter_options = request.GET('filter',None)
+            product_list = Product.objects.select_related('collection').all()
 
-        product = {}
-        series_color = {}
-        for product in product_lst:
-            collection = Collection.objects.get(id=collection_id).name
-            name = product['name']
-            price = product['price']
-            color = product['luggage_color']
-            for obj in Product.objects.filter(name = name and collection_id = collection_id):
-                series_color[obj.luggage_color] = [obj.color_url]
+        color_filter = request.GET.getlist('color',[])
+        price_filter = request.GET.get('price',None)
+        collection_filter = request.GET.getlist('product_collection',[])
+        filtered_obj = Product.objects.all()
+        if len(color_filter) > 0:
+            obj = Product.objects.none()
+            for color in color_filter:
+                obj |= filtered_obj.filter(luggage_color__icontains = color.title())
+            filtered_obj = obj
+        if price_filter:
+            price_filter = price_filter.split('-')
+            min_price = int(price_filter[0].strip()[1:])
+            max_price = int(price_filter[1].strip()[1:])
+            filtered_obj = filtered_obj.filter(price__range = (min_price, max_price))
+        if len(collection_filter) > 0:
+            obj = Product.objects.none()
+            for col in collection_filter:
+                obj |= filtered_obj.filter(collection_id = Collection.objects.get(name = col))
+            print(obj)
+            filtered_obj = obj
+        product_list = filtered_obj
+        result = []
+        if int(category_id) == 1:
+            product_list = product_list.exclude(category_id = 2)
+        else:
+            product_list = product_list.exclude(category_id = 1)
+        for product in product_list:
+            product_info = {}
+            collection = product.collection.name
+            name = product.name
+            price = product.price
+            color = product.luggage_color
+            product_number = product.product_number
+            series_color = []
+            for obj in Product.objects.filter(name = name, collection_id = product.collection.id):
+                series_info = {}
+                series_info['name'] = obj.luggage_color
+                series_info['color_url'] = obj.color_url
                 images = Image.objects.filter(product_id = obj.id)
-                for i in range(2):
-                    series_color[obj.luggage_color].append(images[i].img_url)
-           product['collection'] = collection
-           product['name'] = name
-           product['price'] = price
-           product['luggage_color'] = color
-           product['series_color'] = series_color
-        return JsonResponse({'data':product}, status = 200)
+                series_info['product_img1'] = images[0].img_url
+                series_info['product_img2'] = images[1].img_url
+                series_color.append(series_info)
+            product_info['collection'] = collection
+            product_info['name'] = name
+            product_info['product_number'] = product_number
+            product_info['price'] = price
+            product_info['luggage_color'] = color
+            product_info['series_color'] = series_color
+            result.append(product_info)
+        return JsonResponse({'data':result}, status = 200)
 
 
 
